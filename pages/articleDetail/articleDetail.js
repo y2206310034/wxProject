c5e8d1b5b627051da8f538a9e930468fdb8e2275
yangyuhao <2206310034@qq.com>
2019-03-01 20:36:10 +0800

    add  note

--- pages/articleDetail/articleDetail.js
+++ pages/articleDetail/articleDetail.js
@@ -8,8 +8,11 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // 控制视频播放
     videoPlay:false,
+    // 控制音频播放
     audioPause:true,
+    // 弹幕列表
     danmuList:[{
       text:"这是第一个弹幕",
       time:2,
@@ -21,7 +24,10 @@ Page({
       text:"这是第三个弹幕",
       time:4
     }],
+
+    // 进度条的总长度
     progressWidth:500,
+    // 当前播放的时间
     audioCurTime:0,
 
     //为了在每次访问的时候只记录 一次播放器圆点的位置  
@@ -39,9 +45,6 @@ Page({
     // var audio =  wx.createInnerAudioContext();
     // audio.src="http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46";
     // audio.autoplay=true;
-
-
-
 
   },
   getData:function(id){
@@ -62,6 +65,7 @@ Page({
       videoPlay:true
     })
   },
+  // 点击音乐播放控件使音乐播放
   onAudioPlayTap:function(){
     var playing = this.data.audioPause;
     if(playing){
@@ -73,18 +77,22 @@ Page({
       audioPause:!this.data.audioPause
     })
   },
+  // 对音乐播放器的监控
   onListenerAudio:function(){
     var that =this;
+    // 自然播放结束
     audio.onEnded(function(){
       that.setData({
         audioPause:true,
       })
     })
+    // 暂停音乐
     audio.onPause(function(){
       that.setData({
         audioPause:true
       })
     })
+    // 停止音乐
     audio.onStop(function(){
       that.setData({
         audioPause:true,
@@ -100,9 +108,14 @@ Page({
     var that =this;
     var audioDuration=this.data.articleDetail.audio.duration;
     var audioCurTime,
+        // 播放的百分比
         percent,
+        // 这个参数为了控制progress组件上的percent的值
         progressPercent,
+        // 这个参数控制播放控件小球的位置
         progressCircleLeft;
+    
+    // 播放进度更新事件
     audio.onTimeUpdate(function(){
       audioCurTime = audio.currentTime.toFixed();
       percent = audioCurTime / audioDuration;
@@ -148,6 +161,7 @@ Page({
   },
   getPhoneRadio:function(){
     var radio = 0;
+    // 该api可以获取到设备的相关信息  在这里我们获取到设备的宽度  为了能够进行px到rpx的转化
     wx.getSystemInfo({
       success:function(res){
         var width = res.screenWidth;
@@ -156,6 +170,7 @@ Page({
     })
     return radio;
   },
+  // 将音乐播放器播放封装成一个函数
   audioPlay:function(){
     audio.title=this.data.articleDetail.audio.title;
     audio.src=this.data.articleDetail.audio.src;
